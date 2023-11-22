0f5d14604a69b646c3f289ee15d2c1d7cc09b27c
Sam Biggins <sambiggins@gmail.com>
2023-11-22 07:18:18 -0800

    rename project, menu cleanup
    Signed-off-by: Sam Biggins <sambiggins@gmail.com>

--- gui/crafting/craft_menu.gd
+++ gui/crafting/craft_menu.gd
@@ -4,15 +4,17 @@ extends Control
 @export var menu_title: Label
 @export var recipe_scene: PackedScene
 @export var recipe_slots: Container
-@export var craft_button: Button
+@export var cancel_button: Button
 @export var close_button: Button
+@export var craft_button: Button
 
 var _current_station: CraftStation
 var _recipe_scene_nodes: Array[RecipeMenuItem]
 var _selected_recipe: RecipeMenuItem
 
-@onready var green_outline: StyleBoxFlat = load("res://gui/crafting/green_outline.tres")
-@onready var empty_outline: StyleBoxEmpty = load("res://gui/crafting/empty_style.tres")
+@onready var state_chart: StateChart = $StateChart
+@onready var green_outline: StyleBoxFlat = load("res://assets/styles/green_outline.tres")
+@onready var empty_outline: StyleBoxEmpty = load("res://assets/styles/empty_style.tres")
 
 
 func initialize(craft_station: CraftStation):
@@ -28,19 +30,16 @@ func initialize(craft_station: CraftStation):
 		_recipe_scene_nodes.append(recipe_scene_instance)
 		recipe_scene_instance.set_recipe(craft_station, recipe, recipe_index)
 		recipe_slots.add_child(recipe_scene_instance)
-	_update_signals()
+		recipe_scene_instance.recipe_selected.connect(_on_recipe_selected)
 
 
 func open(craft_station: CraftStation):
-	initialize(craft_station)
 	_current_station = craft_station
-	visible = true
+	state_chart.send_event("show_window")
 
 
 func close():
-	_clear_recipes()
-	_current_station = null
-	visible = false
+	state_chart.send_event("hide_window")
 
 
 func toggle_player_crafting(pawn: Pawn):
@@ -62,26 +61,26 @@ func open_craft_menu(craft_station: CraftStation, inventory: Inventory):
 
 func _clear_recipes():
 	_selected_recipe = null
-	_clear_signals()
 	for recipe_scene_node in _recipe_scene_nodes:
+		recipe_scene_node.recipe_selected.disconnect(_on_recipe_selected)
 		recipe_scene_node.queue_free()
 	_recipe_scene_nodes.clear()
 
 
-func _update_signals():
-	var craft_recipes = get_tree().get_nodes_in_group("craft_recipes")
-	for craft_recipe in craft_recipes:
-		if not craft_recipe.is_connected("recipe_selected", _on_recipe_selected):
-			craft_recipe.connect("recipe_selected", _on_recipe_selected)
-
-
-func _clear_signals():
-	var craft_recipes = get_tree().get_nodes_in_group("craft_recipes")
-	for craft_recipe in craft_recipes:
-		craft_recipe.disconnect("recipe_selected", _on_recipe_selected)
-
-
 func _on_recipe_selected(selected_recipe: RecipeMenuItem):
+	if craft_button.disabled:
+		craft_button.disabled = false
+
+	if _selected_recipe:
+		_selected_recipe.button.add_theme_stylebox_override("normal", empty_outline)
+		_selected_recipe.button.add_theme_stylebox_override("focus", empty_outline)
+
+	_selected_recipe = selected_recipe
+	_selected_recipe.button.add_theme_stylebox_override("normal", green_outline)
+	_selected_recipe.button.add_theme_stylebox_override("focus", green_outline)
+
+
+func _on_queue_item_selected(selected_recipe: RecipeMenuItem):
 	if craft_button.disabled:
 		craft_button.disabled = false
 
@@ -101,3 +100,24 @@ func _on_craft_button_pressed():
 
 func _on_close_button_pressed():
 	close()
+
+
+func _on_cancel_button_pressed():
+	# TODO: Cancel item in queue
+	pass  # Replace with function body.
+
+
+func _on_hidden_state_entered():
+	_clear_recipes()
+	_current_station = null
+	visible = false
+
+
+func _on_visible_state_entered():
+	initialize(_current_station)
+	visible = true
+
+
+func _on_visible_state_processing(_delta):
+	# TODO: Handle window updates
+	pass  # Replace with function body.
